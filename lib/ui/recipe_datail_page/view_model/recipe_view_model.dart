7d1e2e0eb59919075d284e11d9721f2dc5112427
Gabriel Pires <dasaybarreto724@gmail.com>
2025-01-29 13:18:50 -0300

    removendo o desnecessário

--- lib/ui/recipe_datail_page/view_model/recipe_view_model.dart
+++ lib/ui/recipe_datail_page/view_model/recipe_view_model.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:http/testing.dart';
-import 'package:vegan_cibum/data/service/recipe_repository.dart';
 import 'package:vegan_cibum/domain/entities/recipe_information_entity.dart';
 import 'package:vegan_cibum/domain/usecases/get_detail_recipe.dart';
-import 'package:vegan_cibum/ui/recipe_datail_page/view_model/teste_sem_api.dart';
 
 class RecipeDetailViewModel extends ChangeNotifier {
   final GetDetailRecipe repository;
