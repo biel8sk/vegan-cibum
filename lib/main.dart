f6ef8f1fb634e66a935f7138d941b7ee95d6ce57
Gabriel Pires <dasaybarreto724@gmail.com>
2025-02-13 09:38:59 -0300

    adicionando firebase para autenticação

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vegan_cibum/app/app.dart';
+import 'firebase_options.dart';
+/// // ...
+/// await Firebase.initializeApp(
+///   options: DefaultFirebaseOptions.currentPlatform,
+/// );
 
-
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
