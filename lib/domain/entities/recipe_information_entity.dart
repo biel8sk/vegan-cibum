4da9d9fe4afc580ebc9e8685b5223421a9b4c5ce
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-02 08:12:27 -0300

    adicionando classe ao invés de tipo primitivo

--- lib/domain/entities/recipe_information_entity.dart
+++ lib/domain/entities/recipe_information_entity.dart
@@ -1,10 +1,11 @@
 import 'package:vegan_cibum/domain/entities/nutrition_entity.dart';
+import 'package:vegan_cibum/domain/entities/steps_recipe.dart';
 
 class RecipeInformation {
   final int id;
   final String title;
   final String image;
-  final String? instructions;
+  final List<StepsRecipe>? instructions;
   final List<String> extendedIngredients;
   final Nutrition? nutrition;
   final String? summary;
