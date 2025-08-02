b3cf7d5766982fe1874a24789fa551be64faf291
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-02 08:15:57 -0300

    instructions widget

---
+++ lib/ui/recipe_datail_page/widgets/instructions_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:vegan_cibum/domain/entities/steps_recipe.dart';
+
+class InstructionsWidget extends StatelessWidget {
+  final StepsRecipe step;
+  const InstructionsWidget({ super.key, required this.step });
+
+   @override
+   Widget build(BuildContext context) {
+       return ListTile(
+        leading: CircleAvatar(
+          child: Text(step.id.toString()),
+        ),
+        title: Text(step.content),
+       );
+  }
+}
