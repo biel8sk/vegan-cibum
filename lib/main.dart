06fcb01481943703ccaba835cc77f4491c49f8ca
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-02 08:11:27 -0300

    removendo código morto

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vegan_cibum/app/app.dart';
 import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
+
 
 void main() async {
   await Firebase.initializeApp(
