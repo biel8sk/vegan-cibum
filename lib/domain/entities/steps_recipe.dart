eb2400f5ebdabb7e1f8a6926f5228d66b3745a3d
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-07 06:12:09 -0300

    atributo booleano nas etapas da receita para verificar se está completo na hora de cozinhar

--- lib/domain/entities/steps_recipe.dart
+++ lib/domain/entities/steps_recipe.dart
@@ -1,6 +1,7 @@
 class StepsRecipe {
   final int id;
   final String content;
+  final bool isCompleted;
 
-  StepsRecipe({required this.id, required this.content});
+  StepsRecipe({required this.id, required this.content, required this.isCompleted});
 }
