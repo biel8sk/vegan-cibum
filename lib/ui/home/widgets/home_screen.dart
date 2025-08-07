683e75af49e96f99aca1cc6899827ab800ce39a5
Gabriel Pires <dasaybarreto724@gmail.com>
2025-08-07 06:13:22 -0300

    refatorando ui do home page

--- lib/ui/home/widgets/home_screen.dart
+++ lib/ui/home/widgets/home_screen.dart
@@ -11,127 +11,110 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const double myBorder = 30;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: mytheme.primaryColor,
       appBar: const AppBarWidget(),
-      body: Column(
-        children: [
-          //prato do dia
-          Stack(children: [
-            Container(
-              height: 250,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(20)),
-                image: DecorationImage(
-                    image: AssetImage('assets/images/strogonoff_vegano.webp'),
-                    fit: BoxFit.cover),
-              ),
-            ),
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
-                gradient: LinearGradient(
-                  colors: [Colors.black.withOpacity(0.7), Colors.transparent],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //prato do dia
+            Stack(children: [
+              Container(
+                margin: const EdgeInsets.all(12),
+                height: 250,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/strogonoff_vegano.webp'),
+                      fit: BoxFit.cover),
                 ),
               ),
-            ),
-            const Positioned(
-                bottom: 20,
-                left: 40,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextDecorationPlateOfDay(content: "Prato do dia",size:  35),
-                    TextDecorationPlateOfDay(content: "Strogonoff de cogumello",size:  25),
-                    TextDecorationPlateOfDay(content: "50 min | Dificuldade: fácil",size:  18)
-                  ],
-                )),
-          ]),
-          // categorias
+              Container(
+                margin: const EdgeInsets.all(12),
+                height: 250,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  gradient: LinearGradient(
+                    colors: [Colors.black.withOpacity(0.7), Colors.transparent],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+              ),
+              const Positioned(
+                  bottom: 20,
+                  left: 40,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextDecorationPlateOfDay(
+                          content: "Prato do dia", size: 35),
+                      TextDecorationPlateOfDay(
+                          content: "Strogonoff de cogumello", size: 25),
+                      TextDecorationPlateOfDay(
+                          content: "50 min | Dificuldade: fácil", size: 18)
+                    ],
+                  )),
+            ]),
 
-          const Row(
-            children: [
-              Chip(
-                label: Text("Rápidos"),
-              )
-            ],
-          ),          
-          Expanded(
-            child: ListView(
+            const Text(
+              "Top Receitas",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                _buildCategoryFood(
-                  context,
-                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(myBorder))),
-                  title: "Variedade",
-                  description: "Diferentes pratos para você degustar",
-                  icon: Icons.restaurant,
-                ),
-                _buildCategoryFood(
-                  context,
-                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(myBorder))),
-                  title: "Saúde",
-                  description: "Informações nutricionais incluídas",
-                  icon: Icons.health_and_safety_outlined,
-                ),
-                _buildCategoryFood(
-                  context,
-                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(myBorder))),
-                  title: "Confiabilidade",
-                  description: "Instruções de como fazer a receita",
-                  icon: Icons.content_paste_search_rounded,
-                ),
-                _buildCategoryFood(
-                  context,
-                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(myBorder))),
-                  title: "Facilidade",
-                  description: "Pesquise por ingrediente ou nutrição",
-                  icon: Icons.add_task_outlined,
-                ),
+                Container(
+                  padding: const EdgeInsets.all(21),
+                  height: 200,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    color: Colors.lightGreen
+                  ),
+                  child: const Column(
+                    children: [
+                      CircleAvatar(),
+                      Text("Receita 1"),
+                    ],
+                  ),
+                )
               ],
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            // Suas favoritas
+            const Text(
+              "Suas receitas favoritas",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            Row(
+              children: [
+                Container(
+                  child: const Column(
+                    children: [
+                      CircleAvatar(),
+                      Text("Receita 1"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            // Novas receitas
+          ],
+        ),
       ),
     );
   }
 }
 
 
-
-Widget _buildCategoryFood(
-  BuildContext context, {
-  required String title,
-  required String description,
-  required IconData icon,
-  required ShapeBorder shape,
-  
-}) {
-  return Card(
-    shape: shape,
-    child: ListTile(
-      leading: CircleAvatar(
-        child: Icon(icon),
-      ),
-      trailing: CircleAvatar(
-        
-        child: Icon(icon),
-      ),
-      title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25,),),
-      subtitle: Text(description),
-      onTap: () {
-        //navega para página de receitas
-      },
-    ),
-  );
-}
